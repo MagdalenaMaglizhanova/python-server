e9a13c02e4644d3b0e4a1c3fc2d6c10d9b0bb069
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-09-22 08:33:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Разрешаваме CORS за всички домейни
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://food-label-scanner.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
