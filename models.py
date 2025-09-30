92822f3d0b6d805034ead3fe01396da38ad47a22
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-09-30 23:38:05 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,8 @@ class AirQuality(Base):
     __tablename__ = "air_quality"
 
     id = Column(Integer, primary_key=True, index=True)
-    agent_id = Column(String, nullable=True)  # <- ново поле за агента
+    agent_id = Column(String, nullable=True)
+    source = Column(String, nullable=True)       # <- добавено поле
     parameter = Column(String, index=True)
     value = Column(Float, nullable=False)
     timestamp = Column(DateTime, nullable=False)
