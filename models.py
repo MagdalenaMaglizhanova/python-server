e2897f1a2ce022472a4be72fd390c45bf485af98
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-09-30 23:42:52 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,14 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from database import Base
 
-class AirQuality(Base):
-    __tablename__ = "air_quality"
+
+class AirQualityMeasurement(Base):
+    __tablename__ = "air_quality_measurements"  # името на таблицата според предложението
+
 
     id = Column(Integer, primary_key=True, index=True)
-    agent_id = Column(String, nullable=True)
-    source = Column(String, nullable=True)       # <- добавено поле
-    parameter = Column(String, index=True)
-    value = Column(Float, nullable=False)
-    timestamp = Column(DateTime, nullable=False)
+    agent_id = Column(String, nullable=False)          # задължително, защото всеки агент трябва да се идентифицира
+    source = Column(String, nullable=False)            # AQICN / IQAir
+    parameter = Column(String, index=True, nullable=False)  # PM2.5, PM10, Temperature, ...
+    value = Column(Float, nullable=True)              # стойност, може да е NULL ако липсва
+    timestamp = Column(DateTime, nullable=False)       # дата и час на измерването
