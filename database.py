56cca7b7e766afb42445ced51f643907f4446d0f
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-09-30 22:12:17 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # Вече ще има стойност
+DATABASE_URL = os.getenv("postgresql://air_quality_rghr_user:ycp2dcRlLPI1fbKk3ygiS6RdrmMHmkj0@dpg-d3e2i07diees73fqfvhg-a.frankfurt-postgres.render.com/air_quality_rghr")  # Вече ще има стойност
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
